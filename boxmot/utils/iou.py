9d4db996ae2d558da51ce0c529870bc9249fc58d
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:52:55 +0000

    test_lca_code_editing__code-editing-metrics-x407edc70a615fef9__id_153

--- boxmot/utils/iou.py
+++ boxmot/utils/iou.py
@@ -197,16 +197,18 @@ def run_asso_func(func, *args):
     func: The batch function to call (either *iou*_batch or centroid_batch).
     *args: Variable length argument list, containing either bounding boxes and optionally size parameters.
     """
+```python
     if func not in [iou_batch, giou_batch, diou_batch, ciou_batch, centroid_batch]:
-        raise ValueError("Invalid function specified. Must be either '(g,d,c, )iou_batch' or 'centroid_batch'.")
+        raise ValueError("Invalid function specified. Must be either 'iou\_batch', 'giou\_batch', 'diou\_batch', 'ciou\_batch', or 'centroid\_batch'.")
 
-    if func is (iou_batch or giou_batch or diou_batch or ciou_batch):
+    if func in [iou_batch, giou_batch, diou_batch, ciou_batch]:
         if len(args) != 4 or not all(isinstance(arg, (list, np.ndarray)) for arg in args[0:2]):
-            raise ValueError("Invalid arguments for iou_batch. Expected two bounding boxes.")
+            raise ValueError("Invalid arguments for iou\_batch. Expected two bounding boxes.")
         return func(*args)
     elif func is centroid_batch:
         if len(args) != 4 or not all(isinstance(arg, (list, np.ndarray)) for arg in args[:2]) or not all(isinstance(arg, (int)) for arg in args[2:]):
-            raise ValueError("Invalid arguments for centroid_batch. Expected two bounding boxes and two size parameters.")
+            raise ValueError("Invalid arguments for centroid\_batch. Expected two bounding boxes and two size parameters.")
+```
         return func(*args)
     else:
         raise ValueError("No such association method")
